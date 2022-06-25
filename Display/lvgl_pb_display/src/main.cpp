71eb84f66a40106da7375c641a952b0613f8c4d7
LeoDJ <leandro.spaeth@gmail.com>
2022-06-25 23:53:40 +0200

    play around with layout

--- Display/lvgl_pb_display/src/main.cpp
+++ Display/lvgl_pb_display/src/main.cpp
@@ -103,15 +103,31 @@ int main(void)
     bat->draw();
     bat->setValue(90);
 
+    // TODO: color
+    VerticalBar *tempBar = new VerticalBar(cont, 23, 0, 1, 4);
+    tempBar->init("ESC", "%2.0f°C");
+    tempBar->draw();
+    tempBar->setValue(25.9);
+
     NumericalValue *batVolt = new NumericalValue(cont, 1, 0, 3, 1);
     batVolt->init("Batt\nVolt");
     batVolt->draw();
     batVolt->setValue(3.9*12);
 
+    NumericalValue *batAmp = new NumericalValue(cont, 1, 1, 3, 1);
+    batAmp->init("Batt\nAmps");
+    batAmp->draw();
+    batAmp->setValue(43);
+
     Gauge *speed = new Gauge(cont, 4, 0, 4, 4);
     speed->init(0, 50, 10, 2, 30, "km/h");
     speed->draw();
     speed->setValue(33.3);
+
+    Gauge *rpm = new Gauge(cont, 16, 0, 4, 4);
+    rpm->init(0, 1000, 200, 100, 800, "RPM", "%.0f");
+    rpm->draw();
+    rpm->setValue(570);
 
     Graph *graph = new Graph(cont, 8, 0, 8, 2);
     graph->init(60, 
@@ -123,6 +139,37 @@ int main(void)
     for (int i = 0; i < 60; i++) {
         graph->setValue(i*i/32-50, i*i/32);
     }
+
+    NumericalValue *escTemp = new NumericalValue(cont, 1, 2, 3, 1);
+    escTemp->init("ESC\n°C");
+    escTemp->draw();
+    escTemp->setValue(25.9);
+
+    NumericalValue *motTemp = new NumericalValue(cont, 1, 3, 3, 1);
+    motTemp->init("Motor\n°C");
+    motTemp->draw();
+    motTemp->setValue(60.9);
+
+    NumericalValue *mileage = new NumericalValue(cont, 20, 0, 3, 1);
+    mileage->init("kWh/\n100km");
+    mileage->draw();
+    mileage->setValue(5);
+
+    NumericalValue *distance = new NumericalValue(cont, 20, 1, 3, 1);
+    distance->init("Travel.\nkm", "%5.2f");
+    distance->draw();
+    distance->setValue(13.37);
+
+    NumericalValue *range = new NumericalValue(cont, 20, 2, 3, 1);
+    range->init("Est.\nkm", "%5.2f");
+    range->draw();
+    range->setValue(42.23);
+
+    NumericalValue *topSpd = new NumericalValue(cont, 20, 3, 3, 1);
+    topSpd->init("Max\nkm/h");
+    topSpd->draw();
+    topSpd->setValue(66);
+
 
 
 
